8c0db46233aa035acb01176126b317b87b1e6810
Fra <fra24eso@gmail.com>
2025-03-11 09:55:53 +0100

    change base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-
 export default defineConfig({
-  base: '/ghetto/',
+  base: '/',
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://iicd.geoinnova.it',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 });
